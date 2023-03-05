e5fcb16be597dfa6e507720152b85f374bf1618b
Diego Ramos <diegorbrito9@gmail.com>
2023-03-05 20:02:44 -0300

    [curso fundamentos nestjs] fim da aula 'estrutura inicial de um projeto nestjs'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ expandVariables: true })],
   controllers: [AppController],
   providers: [AppService],
 })
