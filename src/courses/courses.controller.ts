ca6344c110aee218e1fca10125c5a197a45b4742
Diego Ramos <diegorbrito9@gmail.com>
2023-03-05 20:27:57 -0300

    [curso fundamentos nestjs] fim da aula 'trabalhando com parametros em rotas'

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get()
+  @Get('findMany')
   findAll() {
     return 'all courses';
+  }
+
+  @Get('findOne/:id')
+  findOne(@Param('id') id: number) {
+    return `course id: ${id}`;
   }
 }
