983201ce5be9f60d764786bef6c17aa54c1a8f34
Diego Ramos <diegorbrito9@gmail.com>
2023-03-05 20:20:26 -0300

    [curso fundamentos nestjs] fim da aula 'trabalhando com controllers'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ expandVariables: true })],
+  imports: [ConfigModule.forRoot({ expandVariables: true }), CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
